e16cb56037af4b738d60a9e37f9170e9ddd048b4
Aashana Shah <aashana.shah@Dexcom.com>
2020-01-15 14:29:25 -0800

    Removed access to reviewsArray and fetching reviews from viewmodel

--- interview-project-master/SNInterview/ViewController.swift
+++ interview-project-master/SNInterview/ViewController.swift
@@ -11,32 +11,13 @@ class ViewController: UIViewController {
     var delegate: CoffeeShopTapDelegate!
     
     @IBOutlet private weak var tableView: UITableView!
-    
-    private let reviews = [
-        CoffeeShop(name:"Lofty", review: "Knowledgeable staff, stacked menu. Trust the Ethiopian in a pour over if you know your flavors. Will be back for the rest of this menu soon.", rating: 4),
-        CoffeeShop(name:"Zumbar", review: "Came to SD for school tour and heading back to the Bay Area after today's final meeting. Was drinking Starbucks the whole trip until my sis recommended this cafe to me. LOVE IT!", rating: 5),
-        CoffeeShop(name:"Blue Bottle", review: "The place was empty the afternoon I came in, so I was able to quickly place an order for their version of a New Orleans cold brew. I received my drink right as my credit card order went through.", rating: 3),
-        CoffeeShop(name:"Bird Rock", review: "When it comes to environment and ambience, this place is amazing! It has a cute lil look, yet looks a bit modern. It's very eye appealing and I would definitely deem Instagramable.", rating: 3),
-        CoffeeShop(name:"Better Buzz Coffee", review: "This coffee shop is really cute. The decor is beautiful, food and coffee is tasty and the service is friendly and quick.", rating: 5)
-    ]
+    private let viewModel = CoffeeShopViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
-        
-        reviews.forEach { coffeeShop in
-            guard let containerView = CoffeeShopItemView.fromNib() as? CoffeeShopItemView else {
-                fatalError("Failed loading CoffeeShopItemView")
-            }
-            
-            containerView.nameLabel.text = coffeeShop.name
-            containerView.reviewLabel.text = coffeeShop.review
-            containerView.ratingLabel.text = "Rating: \(Int(coffeeShop.rating))"
-            
-            containerView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(onTap)))
-        }
         
         delegate = CoffeeShopDetailsHandler()
     }
@@ -49,7 +30,7 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return reviews.count
+        return viewModel.reviews.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
